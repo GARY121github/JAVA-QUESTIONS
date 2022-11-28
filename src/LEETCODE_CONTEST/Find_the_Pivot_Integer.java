4558995361739124c3ec5fbeaf702e389b122a88
GARY <108233340+GARY121github@users.noreply.github.com>
2022-11-28 08:47:06 +0530

    Merge branch 'main' of https://github.com/GARY121github/JAVA-QUESTIONS

--- src/LEETCODE_CONTEST/Find_the_Pivot_Integer.java
+++ src/LEETCODE_CONTEST/Find_the_Pivot_Integer.java
@@ -6,7 +6,7 @@ Given a positive integer n, find the pivot integer x such that:
 
 The sum of all elements between 1 and x inclusively equals the sum of all elements between x and n inclusively.
 Return the pivot integer x. If no such integer exists, return -1. It is guaranteed that there will be at most one pivot
- index for the given input.
+index for the given input.
 
 
 
@@ -15,8 +15,9 @@ Example 1:
 Input: n = 8
 Output: 6
 Explanation: 6 is the pivot integer since: 1 + 2 + 3 + 4 + 5 + 6 = 6 + 7 + 8 = 21.
-Example 2:
 
+
+Example 2:
 Input: n = 1
 Output: 1
 Explanation: 1 is the pivot integer since: 1 = 1.
@@ -34,5 +35,38 @@ Constraints:
 
 */
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Find_the_Pivot_Integer {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        System.out.println(pivot(N));
+    }
+
+//    n/2[2a + (n – 1)d]
+    static int pivot(int N){
+        for(int i = 1 ; i <= N ; i++){
+
+//            LEFT SIDE SUM
+
+            int left = 0;
+            for(int j = 1 ; j <= i ; j++){
+                left += j;
+            }
+
+//            RIGHT SIDE SUM
+            int right = 0;
+            for (int j = i ; j <= N ; j++){
+                right += j;
+            }
+
+            if(left == right){
+                return i;
+            }
+        }
+
+        return -1;
+    }
 }
