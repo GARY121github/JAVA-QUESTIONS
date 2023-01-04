a4edf9178ffd5fc5d7a36c254b8e75525d4664e6
GARY <108233340+GARY121github@users.noreply.github.com>
2023-01-04 12:20:37 +0530

    LEETCODE QUESTION BY CODING BLOCKS

--- src/CODING_BLOCKS_LEETCODE/Fair_Candy_Swap_888.java
+++ src/CODING_BLOCKS_LEETCODE/Fair_Candy_Swap_888.java
@@ -1,23 +1,56 @@
 package CODING_BLOCKS_LEETCODE;
 
+import java.util.*;
+
 public class Fair_Candy_Swap_888 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
     }
 
-    public int[] fairCandySwap(int[] A, int[] B) {
-        int sumA = 0, sumB = 0;
-        for (int i = 0; i < A.length; i++)
-            sumA += A[i];
-        for (int i = 0; i < B.length; i++)
-            sumB += B[i];
-        int dif = (sumA - sumB) / 2;
-        for (int i = 0; i < A.length; i++) {
-            for (int j = 0; j < B.length; j++) {
-                if (A[i] - B[j] == dif)
-                    return new int[]{A[i], B[j]};
-            }
+//    public int[] fairCandySwap(int[] A, int[] B) {
+//        int sumA = 0, sumB = 0;
+//        for (int i = 0; i < A.length; i++)
+//            sumA += A[i];
+//        for (int i = 0; i < B.length; i++)
+//            sumB += B[i];
+//        int dif = (sumA - sumB) / 2;
+//        for (int i = 0; i < A.length; i++) {
+//            for (int j = 0; j < B.length; j++) {
+//                if (A[i] - B[j] == dif)
+//                    return new int[]{A[i], B[j]};
+//            }
+//        }
+//        return null;
+//    }
+
+    private int binarySearch(int[] arr,int target){
+        int s=0,e = arr.length-1;
+
+        while(s<=e){
+            int mid = s+ (e-s)/2;
+            if(arr[mid]==target) return mid;
+            else if(arr[mid]<target) s=mid+1;
+            else e=mid-1;
+
         }
+        return -1;
+    }
+
+    public int[] fairCandySwap(int[] a, int[] b) { //a = Alice , b=Bob
+        int suma=0,sumb=0;
+        for(int i=0;i<a.length;i++)
+            suma+=a[i];
+        for(int i=0;i<b.length;i++)
+            sumb+=b[i];
+
+        int diff = (suma-sumb)/2;
+
+        Arrays.sort(a);
+        for(int num:b){
+            if (binarySearch(a,num+diff) !=-1) return new int[]{num+diff,num};
+        }
+
         return null;
+
     }
 }
