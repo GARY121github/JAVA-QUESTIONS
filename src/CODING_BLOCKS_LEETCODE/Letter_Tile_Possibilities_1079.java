0e1d9d93850ce0df1801d90c1464107e1c3ab0c8
GARY <108233340+GARY121github@users.noreply.github.com>
2023-02-08 10:39:07 +0530

    RECURSION

--- src/CODING_BLOCKS_LEETCODE/Letter_Tile_Possibilities_1079.java
+++ src/CODING_BLOCKS_LEETCODE/Letter_Tile_Possibilities_1079.java
@@ -2,10 +2,32 @@ package CODING_BLOCKS_LEETCODE;
 
 public class Letter_Tile_Possibilities_1079 {
     public static void main(String[] args) {
-        String st = "AAABBC";
+        String st = "AAB";
         int[] arr = new int[26];
-        for(int i = 0 ; i < st.length() ; i++){
+        for (int i = 0; i < st.length(); i++) {
             char ch = st.charAt(i);
+            arr[ch - 65]++;
         }
+        System.out.println(Possibilities(arr , st , 0 , ""));
     }
+
+    static int Possibilities(int[] arr , String st , int n , String ans){
+        if(n == st.length()){
+            System.out.println(ans);
+            return 1;
+        }
+
+        int count = 0;
+        for(int i = 0 ; i < 26 ; i++){
+            if(arr[i] != 0){
+                arr[i]--;
+                count += Possibilities(arr , st , n+1 , ans);
+                count += Possibilities(arr , st , n+1 , ans+ (char) (i+65));
+                arr[i]++;
+            }
+        }
+
+        return count;
+    }
+
 }
--- src/Doubts.java
+++ src/Doubts.java
@@ -5,35 +5,30 @@ import java.util.Scanner;
 
 public class Doubts {
     public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<>();
-//        list.add(1);
-//        list.add(2);
-//        list.add(3);
-//        int[] arr = new int[5];
-//        arr[0] = 1;
-//        arr[1] = 2;
-//
-//        System.out.println(arr);
-//        System.out.println(list);
-
-//        System.out.println(list.get(0));
-        list.add(0 , 1);
-        list.add(0 , 2);
-        list.add(0, 3);
-//        list.add(5 , 4);
-        System.out.println(list);
-
-        System.out.println(list.remove(1));
-        System.out.println(list);
-
-//        arr[0];
-        System.out.println(list.get(1));
-
-//       arr[0] = 1;
-        list.set(1 , 20);
-        System.out.println(list);
-
-        Collections.sort(list);
-        System.out.println(list);
+        int target = 4;
+        possibleWays(target , 0 , "");
     }
+
+    static void possibleWays(int target , int current , String ans){
+        if(current == target){
+            System.out.println(ans);
+            return;
+        }
+
+        if(current > target){
+            return;
+        }
+
+//        CHOOSE 1
+        possibleWays(target , current+1 , ans+"1 ");
+
+//        CHOOSE 2
+        possibleWays(target , current+2 , ans+"2 ");
+
+//        CHOOSE 3
+        possibleWays(target , current+3 , ans+"3 ");
+
+    }
+
+
 }
